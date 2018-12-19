231a8cf005158e9700ed1725528e94cc9b8eb9dd
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2018-12-19 07:24:34 +0300

    change feature icons.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+/*Pseudocode */
+// console.log("hi");
+// 1. Link JS file in index.html.
+// Test to check JS file is linked correctly
+$(document).ready(function() {
+  $("h3").click(function() {
+    alert("This is a header.");
+  });
+});
+
+// - passed
+//
+// /*OBJECTIVES
+// 1. Store users input in an array.
+// 2. Track user's selection in a variable*/
+//
+// var userInput = [];
+// var userSelection = [];
