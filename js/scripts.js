1e54bf0a64f967177a98ca6d4a8c9d01d154f22b
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2018-12-19 09:09:55 +0300

    define radio button names and values.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,14 @@ $(document).ready(function() {
   $("#submit").click(function() {
     var total = 5;
     var score = 0;
+    //fetch user's input
+    var q1 = document.forms["quizForm"] ["q1"].value;
+    var q2 = document.forms["quizForm"] ["q2"].value;
+    var q3 = document.forms["quizForm"] ["q3"].value;
+    var q4 = document.forms["quizForm"] ["q4"].value;
+    var q5 = document.forms["quizForm"] ["q5"].value;
+    
+
 
   });
 });
